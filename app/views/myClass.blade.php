009ca307a542248d0d4f08c6e638cf14856f33ec
Praphaphan Kunlabut <t_numkgen@hotmail.com>
2014-10-27 20:55:43 +0700

    update
    myclass.blade.php

--- app/views/myClass.blade.php
+++ app/views/myClass.blade.php
@@ -3,88 +3,23 @@
 @section('content')
 <div style="margin-left:15px; margin-top:10px;">
 <ul class="nav nav-tabs" role="tablist" id="myTab">
-  <li class="active"><a href="#home" role="tab" data-toggle="tab">Home</a></li>
-  <li><a href="#profile" role="tab" data-toggle="tab">Profile</a></li>
-  <li><a href="#messages" role="tab" data-toggle="tab">Messages</a></li>
-  <li><a href="#settings" role="tab" data-toggle="tab">Settings</a></li>
+  <li class="active"><a href="#check" role="tab" data-toggle="tab">เช็คชื่อ</a></li>
+  <li><a href="#export" role="tab" data-toggle="tab">Export</a></li>
+  <li><a href="#report" role="tab" data-toggle="tab">รายงานผล</a></li>
 </ul>
 
 <div class="tab-content">
-  <div class="tab-pane active" id="home">...</div>
-  <div class="tab-pane" id="profile">
-<form class="form-horizontal" id="editname" role="form" method="post" action="#">
-                <fieldset>
-                  <div class="form-group">
-                    <label for="text" class="col-lg-2 control-label">ชื่อวิชา</label>
-                    <div class="col-lg-10">
-                      <input type="text" class="form-control" id="nameSub" placeholder="Name" required autofocus>
-                    </div>
-                  </div>
-
-           
-
-                  <div class="form-group">
-                    <label for="text" class="col-lg-2 control-label">รหัสวิชา</label>
-                    <div class="col-lg-10">
-                      <input type="text" class="form-control" id="idSub" required autofocus>
-                    </div>
-                  </div>
-                  
-                  <div class="form-group">
-                    <label  class="col-lg-2 control-label">เวลาเรียน</label>
-                    <div class="col-lg-10">
-                      <input type="text" class="form-control" id="startTime" required autofocus>
-                    </div>
-                  </div>
-
-                   <div class="form-group">
-                    <label  class="col-lg-2 control-label">วันที่เรียน</label>
-                    <div class="col-lg-10">
-                      <select class="form-control" id="timestudy">
-						  <option>จันทร์</option>
-						  <option>อังคาร</option>
-						  <option>พุธ</option>
-						  <option>พฤหัสบดี</option>
-						  <option>ศุกร์</option>
-					  </select>
-                    </div>
-                  </div>
-
-                  
-
-                  <div class="form-group">
-                    <label class="col-lg-2 control-label">ห้องเรียน</label>
-                    <div class="col-lg-10">
-                      <input type="text" class="form-control" id="room" required autofocus>
-                    </div>
-                  </div>
-
-                  <div class="form-group">
-                    <label for="inputPassword" class="col-lg-2 control-label">รายละเอียดรายวิชา</label>
-                    <div class="col-lg-10">
-                     <textarea rows="5" cols="50" ></textarea>
-                    </div>
-                  </div>
-
-                  <div class="form-group">
-                    <label  class="col-lg-2 control-label">ไฟล์รายชื่อ</label>
-                    <div class="col-lg-10">
-                      <input type="file" class="flieinput" id="list" required autofocus>
-                    </div>
-                  </div>
-
-                  <div class="form-group" action=" ">
-                    <div class="col-lg-10 col-lg-offset-2">
-                      <button type="submit" class="btn btn-primary">Submit</button>
-                      <button class="btn btn-default">Cancel</button>
-                    </div>
-                  </div>
-                </fieldset>
-              </form>
+  <div class="tab-pane active" id="check">
+    <div stlye="margin-left:20px; margin-top:20px;">
+     <p> 55555555</p>
+    </div>
+  </div>
+  <div class="tab-pane" id="export">
+                 
 
   </div>
+  <div class="tab-pane" id="report">...</div>
   
-  <div class="tab-pane" id="messages">...</div>
 </div>
 			
 </div>
